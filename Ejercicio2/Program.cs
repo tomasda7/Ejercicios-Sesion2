447e3481ba0755dbf4d7bce221990203787e22af
tomasda7 <tomasda7@outlook.com>
2023-02-07 14:05:47 -0300

    Agrego los ejercicios 2 y 3

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,27 @@
+﻿//Tipos: Usando los tipos de variables más adecuados, describe los objetos siguientes:
+//Coche: puertas, ruedas, marca, ITV vigente.
+//Mesa: peso, largo, material, color.
+
+//Coche
+int puertas = 5;
+int ruedas = 4;
+string marca = "Volkswagen";
+bool itvVigente = false;
+
+Console.WriteLine("Coche");
+Console.WriteLine("Marca: " + marca);
+Console.WriteLine("Numero de puertas: " + puertas);
+Console.WriteLine("Numero de ruedas: " + ruedas);
+Console.WriteLine("ITV vigente: " + itvVigente);
+
+//Mesa
+double peso = 30.500d;
+float largo = 2.35f;
+string material = "roble";
+string color = "oscuro";
+
+Console.WriteLine("Mesa");
+Console.WriteLine("Peso: " + peso + " kgs.");
+Console.WriteLine("Largo: " + largo + " mts.");
+Console.WriteLine("Material: " + material);
+Console.WriteLine("Color: " + color);
---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Operadores: Determina los operadores para verificar las siguientes condiciones:
+
+//Un número es mayor o igual a 18
+int numero = 9;
+Console.WriteLine(numero >= 18);
+
+//Un char es ‘a’
+char c = 'c';
+Console.WriteLine(c == 'a');
+
+//Se cumplen dos conciones a la vez (ambas verdaderas)
+Console.WriteLine(c == 'c' && numero > 8);
+
+//Se cumple una de dos condiciones a la vez (una true y otra false)
+Console.WriteLine(numero < 10 || c == 'b');
